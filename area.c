2459c5d073afdbbcd899cc60a0d8973c4659526f
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-04 12:05:59 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,11 @@
+// Program to calculate Area and circumference of the circle
+#include<stdio.h>
+void main()
+{
+float r;
+ float pi=3.14;
+printf(" enter the radias of the circle");
+scanf("%f",&r);
+printf(" enter the circumference of the circle %f ", 2*pi*r);
+printf(" area of the circle%f ", pi*r*r);
+}
